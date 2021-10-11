2a9bacf87794e472743bf99fb3be3c53ae46069c
Rahul Singh <rahul.singh12221998@gmail.com>
2021-10-11 13:04:44 +0530

    fix atc regex

--- src/app/utils/Ivao.js
+++ src/app/utils/Ivao.js
@@ -19,8 +19,8 @@ const ivao = {
   servers: {}
 };
 
-// const atcCallsignRegex = /^V[AEIO][A-Z]{2}_[A-Z_]*$/;
-const atcCallsignRegex = /^[A-Z]{4}_[A-Z_]*$/;
+const atcCallsignRegex = /^V[AEIO][A-Z]{2}_[A-Z_]*$/;
+// const atcCallsignRegex = /^[A-Z]{4}_[A-Z_]*$/;
 const indianAirspaceRegex = /^V[AEIO][A-Z]{2}$/;
 
 function formatDate(date) {
@@ -232,8 +232,8 @@ module.exports = class Ivao {
             t[22].length === 2
               ? `00:${t[22].substring(0, 2)}z`
               : t[22].length === 3
-              ? `0${t[22].substring(0, 1)}:${t[22].substring(1, 3)}z`
-              : `${t[22].substring(0, 2)}:${t[22].substring(2, 4)}z`,
+                ? `0${t[22].substring(0, 1)}:${t[22].substring(1, 3)}z`
+                : `${t[22].substring(0, 2)}:${t[22].substring(2, 4)}z`,
           connectionStartTime: new Date(formatDate(t[37])),
           connectionMilliseconds: new Date() - new Date(formatDate(t[37]))
         };
